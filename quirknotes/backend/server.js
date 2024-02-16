387b0a961526058f3132cfaef65a1e2ac6ad9552
Nathanael Robbins <nejrobbins@gmail.com>
2024-02-16 16:25:27 -0500

    fix tests

--- quirknotes/backend/server.js
+++ quirknotes/backend/server.js
@@ -44,7 +44,7 @@ app.get("/getAllNotes", express.json(), async (req, res) => {
     // Find notes with username attached to them
     const collection = db.collection(COLLECTIONS.notes);
     const data = await collection.find().toArray();
-    res.json({ response: [] });
+    res.json({ response: data });
   } catch (error) {
     res.status(500).json({error: error.message})
   }
@@ -170,7 +170,7 @@ app.patch('/updateNoteColor/:noteId', express.json(), async (req, res) => {
   try {
       const collection = db.collection('notes');
       await collection.updateOne({ _id: new ObjectId(noteId) }, { $set: { color } });
-      res.json({ message: 'Note color updated successfully.' });
+      res.json({ response: 'Note color updated successfully.' });
   } catch (error) {
       res.status(500).json({ error: error.message });
   }
